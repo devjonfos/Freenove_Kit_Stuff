ecb42db7b0f2d772ce9b1d84e35dc9311addcc9c
devjonfos <jonfos@devjonfos.net>
2020-10-09 19:40:00 -0700

    Removed tabs and check spacing

--- Code/C_Code/01.1.1_Blink/Blink-break.c
+++ Code/C_Code/01.1.1_Blink/Blink-break.c
@@ -13,13 +13,13 @@
 #include <signal.h>
 
 // See http://wiringpi.com/pins/
-#define  ledPin    0	//define the led pin number
+#define  ledPin    0  //define the led pin number
 
 // Catch the CTRL-C interrupt and turn LED off
 void interruptHandler(int signal)
 {
   wiringPiSetup();
-	pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
   
   printf("Caught break: LED now off\n");
@@ -28,24 +28,24 @@ void interruptHandler(int signal)
 }
 
 void main(void)
-{	
-	printf("Program is starting ... \n");
-	
-	wiringPiSetup();	                //Initialize wiringPi.
+{  
+  printf("Program is starting ... \n");
+  
+  wiringPiSetup();                  //Initialize wiringPi.
 
   signal(SIGINT, interruptHandler); //Catch the CTRL-C
-	
-	pinMode(ledPin, OUTPUT);          //Set the pin mode
-	printf("Using pin%d\n",ledPin);	  //Output information on terminal
+  
+  pinMode(ledPin, OUTPUT);          //Set the pin mode
+  printf("Using pin%d\n",ledPin);   //Output information on terminal
 
-	while(1)
+  while(1)
   {
-		digitalWrite(ledPin, HIGH);     //Make GPIO output HIGH level
-		printf("led turned on >>>\n");	//Output information on terminal
-		delay(1000);						        //Wait for 1 second
-		digitalWrite(ledPin, LOW);      //Make GPIO output LOW level
-		printf("led turned off <<<\n");	//Output information on terminal
-		delay(1000);						        //Wait for 1 second
-	}
+    digitalWrite(ledPin, HIGH);     //Make GPIO output HIGH level
+    printf("led turned on >>>\n");  //Output information on terminal
+    delay(1000);                    //Wait for 1 second
+    digitalWrite(ledPin, LOW);      //Make GPIO output LOW level
+    printf("led turned off <<<\n"); //Output information on terminal
+    delay(1000);                    //Wait for 1 second
+  }
 }
 
