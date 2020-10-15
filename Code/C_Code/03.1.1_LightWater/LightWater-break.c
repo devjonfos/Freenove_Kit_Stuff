82de3e013edb2ce5cdc6e8bebba0cf1273215883
devjonfos <jonfos@devjonfos.net>
2020-10-14 18:26:38 -0700

    Added resetLEDbar() function and cleaned up code

--- Code/C_Code/03.1.1_LightWater/LightWater-break.c
+++ Code/C_Code/03.1.1_LightWater/LightWater-break.c
@@ -1,11 +1,14 @@
 /**********************************************************************
 * Filename    : LightWater-break.c
 * Description : Use LEDBar Graph(10 LED) 
-*               Added code to reset LEDBar
+*               Added code to catch CTRL-C break and reset LEDBar
 * Authors     : www.freenove.com and
 *               Jon Foster (jonfos@devjonfos.net)
 * License     : CC BY-NC-SA 3.0
 * Modification: 14-Oct-2020
+*
+* Note - If wired according to the Tutorial, the LEDs turn ON
+*        when the GPIO output is sent LOW in the program. 
 **********************************************************************/
 
 #include <wiringPi.h>
@@ -15,19 +18,18 @@
 #include <signal.h>
 
 #define ledCounts 10
-int pins[ledCounts] = {0,1,2,3,4,5,6,8,9,10};
+int pins[ledCounts] = {0,1,2,3,4,5,6,8,9,10}; //use 'gpio readall' for pinouts
 
-// Catch the CTRL-C interrupt and reset LED bar
-void interruptHandler(int signal)
+// Reset LED bar
+void resetLEDbar(void)
 {
   int i;
-  printf("Caught break: Resetting LED Bar\n");
 
   wiringPiSetup(); //Initialize wiringPi.
 
   for(i=0;i<ledCounts;i++) //Set pinMode output; pins on
   {
-    pinMode(pins[i], OUTPUT);    
+    pinMode(pins[i], OUTPUT);
     digitalWrite(pins[i],LOW);
   }
 
@@ -37,6 +39,14 @@ void interruptHandler(int signal)
   {
     digitalWrite(pins[i],HIGH);
   }
+}
+
+// Catch the CTRL-C interrupt and reset LED bar
+void interruptHandler(int signal)
+{
+  printf("Caught break: Resetting LED Bar\n");
+
+  resetLEDbar();
 
   exit(0);
 }
@@ -49,13 +59,18 @@ int main(void)
 
   printf("Program is starting ... \n");
 
+  resetLEDbar();
+
+/* Duplicated code in reset function
   wiringPiSetup(); //Initialize wiringPi.
   
   for(i=0;i<ledCounts;i++) //Set pinMode for all led pins to output
   {
     pinMode(pins[i], OUTPUT);    
   }
+*/
 
+  // Infinite loop sequencing LEDs
   while(true)
   {
     for(i=0;i<ledCounts;i++) //Move led(on) from left to right
